464705a9dff794597f2fe3eb54eff628af439b29
Alaina Barnes <acbarnes1@my.waketech.edu>
2026-03-24 18:18:08 -0400

    random quote generator update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1>Random Quote Generator</h1>
+    <!-- Display the quote or a loading message -->
+    <p v-if="loading">Loading...</p>
+    <p v-else>"{{ quote }}"</p>
+    <p class="author">— {{ author }}</p>
+    <!-- The button that triggers the API call -->
+    <button @click="getQuote">Get New Quote</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      quote: "",
+      author: "",
+      loading: false,
+    };
+  },
+  methods: {
+    async getQuote() {
+      this.loading = true;
+      try {
+        const response = await axios.get(
+          "https://quotes15.p.rapidapi.com/quotes/random/?language_code=en",
+          {
+            headers: {
+              "x-rapidapi-host": "quotes15.p.rapidapi.com",
+              "x-rapidapi-key":
+                "0171245e2amsh2c70c992848764bp176fc5jsnab5ca15885d5",
+            },
+          },
+        );
+
+        // Update the quote with the response data
+        this.quote = response.data.content;
+        this.author = response.data.originator.name;
+      } catch (error) {
+        console.error("Error fetching quote:", error);
+        this.quote = "Oops! Something went wrong.";
+        this.author = "";
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+  mounted() {
+    this.getQuote();
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.container {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Arial, sans-serif;
+  margin-top: 50px;
+}
+button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 16px;
+}
+.author {
+  font-style: italic;
+  color: #555;
+  margin-top: 5px;
 }
 </style>
